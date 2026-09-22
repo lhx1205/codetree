4a89b7201c936f61893e63771e633c746aea4aa5
lhx1205 <lhx1205@gmail.com>
2026-09-22 12:29:49 +0900

    [Passed] 삼항연산자 (49ms, 16MB)

--- trail1/삼항연산자/ternary-operator.py
+++ trail1/삼항연산자/ternary-operator.py
@@ -1,8 +1,9 @@
 N = int(input())
-grade = 0
-if N == 100:
-    grade == 0
-    if grade == 0:
-        print("pass")
-else:
-    print("failure")
+print("pass" if N == 100 else "failure")
+# grade = 0
+# if N == 100:
+#     grade == 0
+#     if grade == 0:
+#         print("pass")
+# else:
+#     print("failure")
