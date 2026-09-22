9dab6cba0346ccb3e7d6d54fb8e346dfec744663
lhx1205 <lhx1205@gmail.com>
2026-09-22 16:48:31 +0900

    [Passed] 4번의 크기 비교 (46ms, 16MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,6 @@
+A = int(input())
+B,C,D,E = list(map(int,input().split()))
+print(1) if A > B else print(0)
+print(1) if A > C else print(0)
+print(1) if A > D else print(0)
+print(1) if A > E else print(0)
