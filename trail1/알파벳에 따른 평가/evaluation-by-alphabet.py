2b1869b4e7b927409ae4c67fbf0d65db6f8ae5ff
lhx1205 <lhx1205@gmail.com>
2026-09-22 15:14:02 +0900

    [Passed] 알파벳에 따른 평가 (51ms, 16MB)

---
+++ trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+N = input()
+if N == "S":
+    print("Superior")
+elif N == "A":
+    print("Excellent")
+elif N == "B":
+    print("Good")
+elif N== "C":
+    print("Usually")
+elif N == "D":
+    print("Effort")
+else:
+    print("Failure")
